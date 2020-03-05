538ddbde0310034997a9ce5e36504318e93bd956
luhc228 <luhengchang228@gmail.com>
2020-03-05 16:48:33 +0800

    add selection-sort

---
+++ selection-sort.js
@@ -0,0 +1,23 @@
+function selectionSort(arr) {
+  let len = arr.length;
+  let minIndex, temp;
+
+  for (let i = 0; i < len; i++) {
+    minIndex = i
+
+    for (let j = i + 1; j < len; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j
+      }
+    }
+
+    temp = arr[i];
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp
+  }
+
+  return arr
+}
+
+const arr = [3, 6, 1, 2, 8]
+console.log(selectionSort(arr))
