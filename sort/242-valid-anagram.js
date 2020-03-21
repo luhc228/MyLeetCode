8132ae26258ada2521408998544b6693b7e19a9c
luhc228 <luhengchang228@gmail.com>
2020-03-21 15:34:12 +0800

    update problem 242

--- sort/242-valid-anagram.js
+++ sort/242-valid-anagram.js
@@ -1,4 +1,19 @@
 /**
+ * 方法一
+ * 先分别对两个字符串进行排序，然后比较他们是否一样
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  let sArr = s.split(''), tArr = t.split('')
+  sArr.sort()
+  tArr.sort()
+  return sArr.toString() === tArr.toString()
+};
+
+/**
+ * 使用hash + 计数排序
  * @param {string} s
  * @param {string} t
  * @return {boolean}
