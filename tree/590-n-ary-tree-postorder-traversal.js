7cc6e257e660b61e62d90de2b3e6d6dce452e0b9
luhc228 <luhengchang228@gmail.com>
2020-03-22 16:19:53 +0800

    update problem-590

--- tree/590-n-ary-tree-postorder-traversal.js
+++ tree/590-n-ary-tree-postorder-traversal.js
@@ -27,3 +27,73 @@ function helper(node, res) {
 
   res.push(node.val)
 }
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var postorder = function (root) {
+  const WHITE = 0
+  const GRAY = 1
+
+  let stack = [[WHITE, root]]
+  let res = []
+
+  while (stack.length) {
+    const [color, node] = stack.pop()
+    if (node) {
+      if (color === WHITE) {
+        if (node.children) {
+          stack.push([GRAY, node])
+          for (let i = node.children.length - 1; i >= 0; i--) {
+            stack.push([WHITE, node.children[i]])
+          }
+        }
+      } else {
+        res.push(node.val)
+      }
+    }
+  }
+
+  return res
+};
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var postorder = function (root) {
+  if (root == null) {
+    return []
+  }
+
+  let res = []
+  let stack = [root]
+
+  while (stack.length) {
+    node = stack.pop()
+    if (node) {
+      res.push(node.val)
+    }
+    for (let c of node.children) {
+      stack.push(c)
+    }
+  }
+
+  res.reverse()
+  return res
+};
